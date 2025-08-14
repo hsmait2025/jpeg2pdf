8dda74cf86432c0d4d35ef94a547e3fe297e3764
hsmait2025 <hsmait2025@gmail.com>
2025-08-14 14:02:56 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "compressor-cache-v2"; // bumped version
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png",
+        "https://cdn.jsdelivr.net/npm/pdf-lib/dist/pdf-lib.min.js" // needed for PDF Splitter offline
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
